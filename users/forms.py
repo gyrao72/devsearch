0d1e5586b5c64a951fa4c0020fa5baee6c975db3
Gajendar <gyrao72@gmail.com>
2021-07-10 23:35:55 +0530

    user account and edit profile

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,7 @@
 from django.forms import ModelForm, fields
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-
+from .models import Profile
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
@@ -14,6 +14,22 @@ class CustomUserCreationForm(UserCreationForm):
         # Customizing form  
     def __init__(self,*args,**kwargs):
         super(CustomUserCreationForm,self).__init__(*args,**kwargs)
+
+        for name,field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
+
+
+
+class ProfileForm(ModelForm):
+    class Meta:
+        model=Profile
+        fields=['name','email','username','location','bio','short_intro','profile_image',
+        'social_github','social_twitter','social_linkedin','social_stackoverflow',
+        'social_website'
+        ]
+
+    def __init__(self,*args,**kwargs):
+        super(ProfileForm,self).__init__(*args,**kwargs)
 
         for name,field in self.fields.items():
             field.widget.attrs.update({'class': 'input'})
