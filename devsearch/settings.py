15189d5794d9da0c45fc30973b4e974b74601e04
Gajendar <gyrao72@gmail.com>
2021-07-19 22:27:39 +0530

    fixed bugs

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -157,7 +157,7 @@ EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
 EMAIL_HOST_USER='devsearch.test@gmail.com'
-EMAIL_HOST_PASSWORD='flaqfvnothbjlyon'
+EMAIL_HOST_PASSWORD=''
 
 CORS_ALLOW_ALL_ORIGINS = True
 
