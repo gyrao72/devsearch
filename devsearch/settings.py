ca916c5152f9db587a352a9d761ccdc76f9646be
Gajendar <gyrao72@gmail.com>
2021-07-20 20:29:04 +0530

    c

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -194,6 +194,8 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'static/images')
 # FOR SERVER
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
