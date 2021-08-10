870bd08856ebbf6eb17dde95aa8739bb178088ca
Gajendar <gyrao72@gmail.com>
2021-08-10 11:23:35 +0530

    db changed

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -112,23 +112,23 @@ WSGI_APPLICATION = 'devsearch.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'devsearch',
-        'USER':'postgres',
-        'PASSWORD':os.environ.get('DB_PASS'),
-        'HOST':os.environ.get('HOST_URI'),
-        'PORT':'5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'devsearch',
+#         'USER':'postgres',
+#         'PASSWORD':os.environ.get('DB_PASS'),
+#         'HOST':os.environ.get('HOST_URI'),
+#         'PORT':'5432',
+#     }
+# }
 
 
 # Password validation
